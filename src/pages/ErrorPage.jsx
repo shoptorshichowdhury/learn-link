b8ca0d3da0a6ea9e0ea38f04bf858be3ead409de
shoptorshichowdhury <shoptorshichowdhury.info@gmail.com>
2024-12-26 23:22:20 +0600

    error page updated

--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -2,9 +2,9 @@ import { Link } from "react-router-dom";
 
 const ErrorPage = () => {
   return (
-    <div className="flex items-center justify-center min-h-screen">
+    <div className="flex flex-col items-center justify-center min-h-screen">
       <h2 className="text-lg md:text-5xl font-bold">Page Not Found</h2>
-      <p className="text-lg md:text-3xl text-red-600">Error: 404</p>
+      <p className="text-lg md:text-3xl text-red-600 font-medium">Error: 404</p>
       <Link to="/">
         <button className="btn btn-neutral">Get Back Home</button>
       </Link>
